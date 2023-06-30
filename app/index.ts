b285e460417deb7f137927ee66414899ca156c52
Peter Gassner <peter@gassner.io>
2023-06-30 16:16:24 +0200

    Reproduction 3

--- app/index.ts
+++ app/index.ts
@@ -1,4 +1,17 @@
-import "fp-ts"; // Fails if this import is here, succeeds if it isn't
+/**
+ * This file does not compile if the `import "fp-ts" line is present.
+ *
+ * The error reads:
+ *
+ *     $ npx tsc -p app/tsconfig.json
+ *     lib/box.ts:18:40 - error TS2344: Type '"Box"' does not satisfy the constraint 'keyof URItoKind<any>'.
+ *
+ *     18 export const Pointed: pointed.Pointed1<URI> = {
+ *                                             ~~~
+ *
+ *     Found 1 error in lib/box.ts:18
+ */
+import "fp-ts";
 import * as box from "lib/box";
 
 console.log(box.Pointed.of(3))
